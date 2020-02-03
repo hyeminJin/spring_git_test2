2e92bde472f5cb46c5f3a5ea413ce962a1bff187
hyeminjin <wlsgpals2@gmail.com>
2020-02-03 10:38:19 +0900

    기본

--- src/main/java/edu/bit/ex/HomeController2.java
+++ src/main/java/edu/bit/ex/HomeController2.java
@@ -11,10 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//1번추가
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController2 {
 	
